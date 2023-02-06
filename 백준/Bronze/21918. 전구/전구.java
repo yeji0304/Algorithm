df9dd42f83dfb65c58588416c33d9b16c0c26c28
yeji0304 <75303297+yeji0304@users.noreply.github.com>
2023-02-06 17:13:36 +0900

    [Bronze II] Title: 전구, Time: 512 ms, Memory: 33920 KB -BaekjoonHub

---
+++ 백준/Bronze/21918. 전구/전구.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] light = new int[N+1];
+		
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=1; i<=N; i++) {
+			light[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int n1 = Integer.parseInt(st.nextToken());
+			int n2 = Integer.parseInt(st.nextToken());
+			int n3 = Integer.parseInt(st.nextToken());
+			
+			if(n1 == 1) {
+				light[n2] = n3;
+			} else if (n1 == 2) {
+				for(int j=n2; j<=n3; j++) {
+					if(light[j] == 1)
+						light[j] = 0;
+					else
+						light[j] = 1;
+				}
+				
+			} else if (n1 == 3) {
+				for(int j=n2; j<=n3; j++) {
+					light[j] = 0;
+				}
+			} else {
+				for(int j=n2; j<=n3; j++) {
+					light[j] = 1;
+				}
+			}
+		}
+		for(int i=1; i<=N; i++)
+			System.out.print(light[i] + " ");
+	}
+}
