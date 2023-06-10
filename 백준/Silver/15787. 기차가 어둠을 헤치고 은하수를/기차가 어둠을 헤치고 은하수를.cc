93f4e4bede80dc5f63b32e8f765da89d9c9c35f9
yeji0304 <75303297+yeji0304@users.noreply.github.com>
2023-06-10 16:42:11 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 68 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int main() {
+	
+	int N, M;
+	cin >> N >> M;
+
+	int seat;
+	vector<int> train(N + 1, 0);
+
+	for (int i = 0; i < M; i++) {
+		int order, trainNum;
+		cin >> order >> trainNum;
+
+		switch (order) {
+			case 1: {
+				cin >> seat;
+				train[trainNum] |= 1 << seat;
+				break;
+			}
+			case 2: {
+				cin >> seat;
+				train[trainNum] &= ~(1 << seat);
+				break;
+			}
+			case 3: {
+				train[trainNum] <<= 1;
+				train[trainNum] &= ((1 << 21) - 1);
+				break;
+			}
+			case 4: {
+				train[trainNum] >>= 1;
+				train[trainNum] &= ~1;
+				break;
+			}
+		}
+	}
+
+	set<int> s;
+	for (int i = 1; i <= N; i++) {
+		s.insert(train[i]);
+	}
+	cout << s.size();
+}
